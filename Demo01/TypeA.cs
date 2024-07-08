e0220689eec82c5a5e7231f1ff3f536d3150d046
xcite <xcite@LAPTOP-LQD1GQ1C>
2024-07-08 21:26:58 +0300

    Add project files.

---
+++ Demo01/TypeA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+   public class TypeA
+    {
+        private int x;
+        internal int y;
+       public int z;
+       //public TypeA() { 
+       // TypeB obj = new TypeB();
+       //         }
+        
+
+    }
+}
---
+++ DemoOOP/Program.cs
@@ -0,0 +1,74 @@
+﻿using Demo01;
+namespace DemoOOP
+{
+    //public class Employee
+    //{
+    //   public string name;
+    //    int age;
+    //   public Permissions permissions;
+    //}
+
+    //[Flags]
+    //public enum Permissions : byte
+    //{
+    //    Delete = 1,
+    //    Execute = 2,
+    //    Read = 4,
+    //    Write = 8
+    //}
+    internal class Program { 
+//    { static void DoSomeProtectiveCode()
+//        {
+//         try
+//            {
+//                int x, y, z;
+//                bool flag;
+
+//                do { flag = int.TryParse(Console.ReadLine(), out x); } while (!flag);
+//                do { flag = int.TryParse(Console.ReadLine(), out y); } while (!flag || y == 0);
+
+//                z = x / y;
+//                int[] numbers = { 1, 2, 3, 4, 5 };
+//                if (numbers?.Length > 10)
+//                {
+//                    numbers[10] = 100;
+//                }
+
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+//}
+//static void DoSomeCode()
+//        {   try
+//            {
+//                int x, y, z;
+//                x = int.Parse(Console.ReadLine());
+//                y = int.Parse(Console.ReadLine());
+//                z = x / y;
+//                int[] numbers = { 1, 2, 3, 4, 5 };
+//                numbers[10] = 100;
+
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+//        }
+            static void Main(string[] args)
+        {
+
+            //DoSomeCode()
+            //DoSomeProtectiveCode()
+            //Employee employee = new Employee();
+            //employee.permissions = (Permissions)5;
+            //Console.WriteLine(employee.permissions);
+            //if ((employee.permissions & Permissions.Read) == Permissions.Read)
+            //{
+            //    Console.WriteLine("Read is existed");
+            //}
+            //else { Console.WriteLine("Read is not existed"); }
+        }
+
+    } }
